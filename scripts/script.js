1bffe2f5f0d7ae0d9aeec2d6442543d0f43ac07d
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-06-02 17:34:55 -0300

    1.3

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,17 @@ var nome = prompt('Bem-vindo ao site! Qual é o seu nome?');
 var res = document.getElementById('boas-vindas')
 res.innerHTML = `<p id="inicio" >Bem-vindo, <strong>${nome}</strong>, este é um site destinado a te ajudar naquele momento em que você já não sabe mais o que fazer no Minecraft, por exemplo, ou então, quando não consegue ser criativo o suficiente para pensar em uma carreira no FIFA, dentre outros casos. Por isso, criei este site.</p>`;
 var footer = document.getElementsByTagName('footer')[0];
+var obs0 = document.getElementsByClassName('obs')[0];
+var obs1 = document.getElementsByClassName('obs')[1];
 
 function minecraft() {
     footer.style.display = 'block';
+    obs1.style.display = 'none';
+    obs0.style.display = 'none';
 }
 
 function fifa() {
     footer.style.display = 'block';
+    obs1.style.display = 'none';
+    obs0.style.display = 'none';
 }
